df608e67d1c01f92a0514c7a95e7d76aade11623
EmreKaya0860 <h.emrekaya2207@gmail.com>
2026-02-02 16:27:51 +0300

    Initial commit - API infrastructure

---
+++ HEKStore.WebApi/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using HEKStore.Application.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HEKStore.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("success")]
+        public IActionResult Success()
+        {
+            return Ok(ApiResponse<string>.SuccessResponse(
+                "API çalışıyor 🚀"));
+        }
+
+        [HttpGet("error")]
+        public IActionResult Error()
+        {
+            throw new Exception("Boom!");
+        }
+    }
+}
---
+++ HEKStore.WebApi/Program.cs
@@ -0,0 +1,37 @@
+using HEKStore.WebApi.Middlewares;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Serilog
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//middleware
+app.UseMiddleware<ExceptionMiddleware>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
